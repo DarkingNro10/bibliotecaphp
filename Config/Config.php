7c967f3b486b0337c8bbe3bc6d56266c618110f1
DarkingNro10 <marcosfelipevaleriano17@gmail.com>
2024-07-04 14:47:45 -0500

    primer

---
+++ Config/Config.php
@@ -0,0 +1,9 @@
+<?php
+const base_url = "http://localhost/BIBLIOTECA-PHP/";
+const host = "biblioteca.c4rm5ableevq.us-east-1.rds.amazonaws.com";
+const user = "admin";
+const pass = "admin123456789";
+const port = "3306";
+const db = "biblioteca";
+const charset = "charset=utf8";
+?>
